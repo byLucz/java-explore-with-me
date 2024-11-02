e1b112e7b3514b849e1d32fcf3d69bea9d6c936b
Dobronezsky <dobronezsky@ya.ru>
2024-11-02 18:36:17 +0300

    feat: based entities transformed to biz logics

---
+++ main-service/src/main/java/ru/practicum/model/event/dto/UpdateEventRequest.java
@@ -0,0 +1,27 @@
+package ru.practicum.model.event.dto;
+
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UpdateEventRequest {
+    @Size(min = 20, max = 2000)
+    private String annotation;
+    private Integer category;
+    @Size(min = 20, max = 7000)
+    private String description;
+    private String eventDate;
+    private LocationDto location;
+    private Boolean paid;
+    @PositiveOrZero
+    private Integer participantLimit;
+    private Boolean requestModeration;
+    private String stateAction;
+    @Size(min = 3, max = 120)
+    private String title;
+}
