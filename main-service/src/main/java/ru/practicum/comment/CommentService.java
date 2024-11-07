88dc6c80112e73a44c451966c60607e95a37ae43
dobronezsky <dobronezsky@ya.ru>
2024-11-07 04:16:16 +0300

    fix: improved code by actions check

--- main-service/src/main/java/ru/practicum/comment/CommentService.java
+++ main-service/src/main/java/ru/practicum/comment/CommentService.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface CommentService {
     CommentDto addComment(Long eventId, Long userId, String text);
+
     List<CommentDto> getCommentsByEvent(Long eventId);
+
     List<CommentDto> getCommentsByUser(Long userId);
+
     void deleteComment(Long commentId, Long userId);
 }
